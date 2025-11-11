4f77bf8e44a1704f7244cef57d0e48b400d98f12
Kannanperiyakaruppan <kannan827070@gmail.com>
2025-11-11 16:56:58 +0530

    app.java

--- app.java
+++ app.java
@@ -2,3 +2,11 @@ i am writing a java code
 java is a open source
 java is code that it will be develope to create a mobile application and web application 
 
+
+  Soruce code from java dev 2 
+  public class HelloWorld {
+    public static void main(String[] args) {
+        // This line prints "Hello, World!" to the console
+        System.out.println("Hello, World!"); 
+    }
+}
