1180ba38a4a24ebcf143f2e1a452565ca43bb14b
Kannanperiyakaruppan <kannan827070@gmail.com>
2025-11-17 13:31:23 +0530

    Update app.java

--- app.java
+++ app.java
@@ -10,3 +10,14 @@ java is code that it will be develope to create a mobile application and web app
         System.out.println("Hello, World!"); 
     }
 }
+Source code 3 
+  public static void main(String[] args) {
+int[] arr = {64, 34, 25, 12, 22, 11, 90};
+bubbleSort(arr);
+System.out.println("Sorted array:");
+for (int i = 0; i < arr.length; i++) {
+System.out.print(arr[i] + " ");
+}
+}
+}
+  
